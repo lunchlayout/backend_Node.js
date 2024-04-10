528f5d6ede4ce95f883efab489a621eeab1f0511
liminfinity <108136720+liminfinity@users.noreply.github.com>
2024-04-10 10:34:28 +0700

    add api schema

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,18 @@ import express from 'express'
 import yaml from 'node-yaml-config'
 import path from 'path'
 import { IConfig } from './types/config'
-
+import cors from 'cors'
 
 const {server: {host, port}}: IConfig = yaml.load(path.resolve('config.yaml'))
 
 
 const app = express();
+
+app.use(express.json())
+app.use(cors({
+    origin: "*"
+}))
+
 
 app.listen(port, host, () => {
     console.log(`server started on ${port} port and ${host} host`)
